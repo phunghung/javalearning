58873a51f776db9875aa29b9b429496e9c042477
Hungpx <phunghung1105@gmail.com>
2019-03-14 11:26:52 +0700

    bai tap java1

--- lab1/Bai1.java
+++ lab1/Bai1.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Bai1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Input full name: ");
-		String name = sc.nextLine();
-		System.out.println("Input score: ");
-		double score = Double.parseDouble(sc.nextLine());
-		System.out.printf("%s %.1f điểm", name, score);	
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input full name: ");
+        String name = sc.nextLine();
+        System.out.println("Input score: ");
+        double score = Double.parseDouble(sc.nextLine());
+        System.out.printf("%s %.1f điểm", name, score);
+    }
 }
--- lab1/Bai3.java
+++ lab1/Bai3.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class Bai3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		double a, b;
-		try {
-			System.out.println("Nhập cạnh cua lap phuong: ");
-			a = sc.nextDouble();
-			
-			double thetich = Math.pow(a, 3);
-			System.out.println("The tich lap phuong: " + thetich);
-		}
-		catch(Exception ex) {
-			System.out.println("Input error!");
-		}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
 
-	}
+        double a, b;
+        try {
+            System.out.println("Nhập cạnh cua lap phuong: ");
+            a = sc.nextDouble();
+
+            double thetich = Math.pow(a, 3);
+            System.out.println("The tich lap phuong: " + thetich);
+        } catch (Exception ex) {
+            System.out.println("Input error!");
+        }
+
+    }
 }
